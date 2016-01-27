7b9fccff3dedaee1ff482b7adf502f9e8ac2ad4b
Tatiana <starciuc.tatiana@gmail.com>
2016-01-27 10:41:46 +0200

    fixed header

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,14 +2,18 @@
     'use strict';
 
    // variables
-    var $header = BASEMENT_ESCAPE.modules.header;
+    var $header = BASEMENT_ESCAPE.modules.header,
+        $navigation = BASEMENT_ESCAPE.modules.navigation;
 
     (function init() {
         // local variables
 
         $(document).ready(function(){
-            var $fixedElem = $('#fixed--menu');
+            var $fixedElem = $('#fixed--menu'),
+                $hamburger = $('.content--menu__menu img');
+
             $header.fixedHeader($fixedElem);
+            $navigation.toggleMobileMenu($hamburger);
         });
 
     })();
