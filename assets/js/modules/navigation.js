39d950c872ab518f55fafcae730dd436c857820e
Tatiana <starciuc.tatiana@gmail.com>
2016-01-29 17:50:01 +0200

    css issue

--- assets/js/modules/navigation.js
+++ assets/js/modules/navigation.js
@@ -20,14 +20,14 @@
                     "background-color": "rgba(0,0,0,0.8)"
                 });
                 $firstNav.addClass('open');
-                $firstNav.find('ul').addClass('open');
+                $firstNav.find('ul').addClass('open--ul');
                 $firstNav.removeClass('closed');
-                $firstNav.find('ul').removeClass('closed');
+                $firstNav.find('ul').removeClass('closed--ul');
             }else {
                 $firstNav.removeClass('open');
-                $firstNav.find('ul').removeClass('open');
+                $firstNav.find('ul').removeClass('open--ul');
                 $firstNav.addClass('closed');
-                $firstNav.find('ul').addClass('closed');
+                $firstNav.find('ul').addClass('closed--ul');
             }
 
 
