88bed64831cf0ba1329838822486a080463ea14c
Tatiana <starciuc.tatiana@gmail.com>
2016-02-04 09:53:26 +0200

    server.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,17 +2,15 @@
     'use strict';
 
    // variables
-    var $header = BASEMENT_ESCAPE.modules.header,
-        $navigation = BASEMENT_ESCAPE.modules.navigation;
+    var $navigation = BASEMENT_ESCAPE.modules.navigation;
+
 
     (function init() {
         // local variables
 
         $(document).ready(function(){
-            var $fixedElem = $('#fixed--menu'),
-                $hamburger = $('.content--menu__menu img');
+            var $hamburger = $('.content--menu__menu img');
 
-            $header.fixedHeader($fixedElem);
             $navigation.toggleMobileMenu($hamburger);
         });
 
