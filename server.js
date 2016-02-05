01b7283b9847b3feacd85e0ad6f808d5979f78d6
Tatiana <starciuc.tatiana@gmail.com>
2016-02-05 09:01:44 +0200

    redirect joker_room

--- server.js
+++ server.js
@@ -24,17 +24,17 @@ server.engine('hbs', hbs.express3({
 server.set('views', __dirname + '/views');
 
 server.get('/', function(req, res) {
-    console.log(contentJSON);
+    server.engine('hbs', hbs.express3({
+        defaultLayout: __dirname + '/views/layouts/main.hbs',
+        partialsDir: __dirname + '/views/partials',
+        layoutsDir: __dirname + '/views/layouts'
+    }));
     res.render('index', contentJSON);
 
 });
 
 server.get('/joker_room', function(req, res) {
-    server.engine('hbs', hbs.express3({
-        defaultLayout: __dirname + '/views/layouts/joker_room.hbs',
-        partialsDir: __dirname + '/views/partials',
-        layoutsDir: __dirname + '/views/layouts'
-    }));
+    server.engine('hbs', hbs.express3({defaultLayout:  __dirname + '/views/layouts/joker_room.hbs'}));
     res.render('joker_room');
 
 });
